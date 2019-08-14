0ec2db765df9d188c90c6bb10545c00753249bec
Joe Story <joe.story@mailme.ae>
2019-08-14 13:56:31 +0100

    Fixed waypoint upload

--- example/payload_mission/payload_mission.cpp
+++ example/payload_mission/payload_mission.cpp
@@ -301,6 +301,16 @@ int main(int argc, char **argv)
 
     //*** ADD IN A LOITER TIME: MissionItem::set_loiter_time(3) ***//
 
+    //Send home information first
+    mission_items.push_back(make_mission_item(waypoint_array[0].lat,
+                                              waypoint_array[0].lon,
+                                              20.0f,
+                                              waypoint_array[0].speed,
+                                              3.0f,
+                                              false,
+                                              waypoint_array[0].deadline,
+                                              waypoint_array[0].payload));
+
     for (int x = 1; x<numOfWaypoints+1; x++){
         mission_items.push_back(make_mission_item(route_array[x].lat,
                                                   route_array[x].lon,
@@ -311,16 +321,6 @@ int main(int argc, char **argv)
                                                   route_array[x].deadline,
                                                   route_array[x].payload));
     }
-
-    //Send home information last
-    mission_items.push_back(make_mission_item(waypoint_array[0].lat,
-                                              waypoint_array[0].lon,
-                                              20.0f,
-                                              waypoint_array[0].speed,
-                                              3.0f,
-                                              false,
-                                              waypoint_array[0].deadline,
-                                              waypoint_array[0].payload));
 
 
     {
