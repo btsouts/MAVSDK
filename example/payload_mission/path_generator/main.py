b051c1dbf292c025b6e534df185213715f0ec60b
Joe Story <joe.story@mailme.ae>
2019-08-28 11:21:42 +0100

    Added all uniform distribution waypoints

--- example/payload_mission/path_generator/main.py
+++ example/payload_mission/path_generator/main.py
@@ -4,28 +4,28 @@ import math
 earth_radius = 6371000
 
 #Number of waypoints
-size = 5
+size = 20
 
-origin_lat = 47.3977413
-origin_lon = 8.5455941
+origin_lat = 52.210444
+origin_lon = 0.092640
 speed = 5.0
 delivery_time = 5.0
 accel_time = 3.0
 
 #Boundaries (x and y)
-lat_upper_bound_x = 400
+lat_upper_bound_x = 480
 lat_lower_bound_x = 0
-lon_upper_bound_y = 400
+lon_upper_bound_y = 480
 lon_lower_bound_y = 0
 
-#Calculate suitable deadlines using an estimate of the average distance between waypoints (1.04 is an arbitrary number)
+#Calculate suitable deadlines using an estimate of the average distance between waypoints (1.05 is an arbitrary number)
 av_dist = math.sqrt(((lat_upper_bound_x-lat_lower_bound_x)/size)**2+((lon_upper_bound_y-lon_lower_bound_y)/size)**2)
-av_time = ((av_dist/speed)+delivery_time+accel_time)*(1.04**size)
+av_time = ((av_dist/speed)+delivery_time+accel_time)*(1.05**size)
 deadline_upper_bound = (av_time*size)
 deadline_lower_bound = (av_time*size)/2
 
 #Waypoint parameters
-payload_upper_bound = 0.3 #grams
+payload_upper_bound = 0.8 #grams
 payload_lower_bound = 0.1 #grams
 
 lat_x = lat = np.random.uniform(lat_lower_bound_x, lat_upper_bound_x, size)
@@ -60,8 +60,8 @@ print(lat_x)
 print("\nLon Y")
 print(lon_y)
 
-with open("../input.txt", "w+") as text_file:
-    text_file.write("%s 1.380 4 0.12 5350 15.2 5 1 0.95" % size)
+with open("generated_paths/input.txt", "w+") as text_file:
+    text_file.write("%s 1.380 4 0.12 5350 15.2 5 1 1.0" % size)
     for i in range(0,(size)):
         text_file.write("\n{} {} {} {} {} {} {} {}".format(i+1, str(unichr(65+i)), lat[i], lon[i], 20.0, 5, deadline[i], payload[i]))
 
